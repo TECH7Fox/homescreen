f3fbcbc1e5f3fd85044d39d789bf45d9b8a3c742
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2020-08-09 18:57:38 +0200

    added .env config

--- scripts/auto/lights.py
+++ scripts/auto/lights.py
@@ -3,28 +3,34 @@ from datetime import datetime
 import time
 import os
 import json
+import configparser
 
 from astral import LocationInfo
 from astral.sun import sun
 
 city = LocationInfo("Amsterdam", "Netherlands", "Europe/Amsterdam")
 s = sun(city.observer, date=datetime.now())
-alarm = 0
+
+env = configparser.ConfigParser()
 
 while True:
     raw = req.get("http://localhost/api/lights.php")
     json_object = raw.json()
 
+    env.read("/var/www/html/config/.env")
+
     for switch in json_object:
         if datetime.now() >= s['sunset'].replace(tzinfo=None) or datetime.now() <= s['sunrise'].replace(tzinfo=None):
             if switch["value"] == "auto_alarm":
-                if alarm != 1:
-                    os.system("/home/pi/433Utils/RPi_utils/codesend " + switch["on_code"]) #turn on
-                    print("turned on by alarm check")
+                if env["alarm"]["armed"] != 1:
+                    os.system(["paths"]["433Utils"] "/433Utils/RPi_utils/codesend " + switch["on_code"]) #turn on
+                    print("turn light on")
             else:
-                os.system("/home/pi/433Utils/RPi_utils/codesend " + switch["on_code"]) #turn on
+                os.system(env["paths"]["433Utils"] + "/433Utils/RPi_utils/codesend " + switch["on_code"]) #turn on
+                print("turn light on")
         else:
-            os.system("/home/pi/433Utils/RPi_utils/codesend " + switch["off_code"]) #turn off
+            os.system(env["paths"]["433Utils"] + "/433Utils/RPi_utils/codesend " + switch["off_code"]) #turn off
+            print("turn light off")
 
     print("")
     print("Sunrise: " + s['sunrise'].replace(tzinfo=None).strftime("%m/%d/%Y, %H:%M:%S"))
