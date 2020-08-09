01b930478dda2b1af97d6fd57f0aab663ca4e323
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2020-08-09 14:18:31 +0200

    fixed bug

--- scripts/auto/lights.py
+++ scripts/auto/lights.py
@@ -1,5 +1,5 @@
 import requests as req
-import datetime
+from datetime import datetime
 import time
 import os
 import json
@@ -8,6 +8,7 @@ from astral import LocationInfo
 from astral.sun import sun
 
 city = LocationInfo("Amsterdam", "Netherlands", "Europe/Amsterdam")
+s = sun(city.observer, date=datetime.now())
 alarm = 0
 
 while True:
@@ -15,7 +16,7 @@ while True:
     json_object = raw.json()
 
     for switch in json_object:
-        if datetime.datetime.now() >= city.sun['sunset'].replace(tzinfo=None) or datetime.datetime.now() <= city.sun['sunrise'].replace(tzinfo=None):
+        if datetime.now() >= s['sunset'].replace(tzinfo=None) or datetime.now() <= s['sunrise'].replace(tzinfo=None):
             if switch["value"] == "auto_alarm":
                 if alarm != 1:
                     os.system("/home/pi/433Utils/RPi_utils/codesend " + switch["on_code"]) #turn on
@@ -27,13 +28,13 @@ while True:
 
 
     #s = sun(city.observer, date=datetime.date(2009, 4, 22))
-    #if datetime.datetime.now() >= city.sun['sunset'].replace(tzinfo=None) or datetime.datetime.now() <= city.sun['sunrise'].replace(tzinfo=None):
+    #if datetime.datetime.now() >= s['sunset'].replace(tzinfo=None) or datetime.datetime.now() <= s['sunrise'].replace(tzinfo=None):
     #    os.system("/home/pi/433Utils/RPi_utils/codesend 5441") #turn on
     #else:
     #    os.system("/home/pi/433Utils/RPi_utils/codesend 5444") #turn off
 
     print("")
-    print("Sunrise: " + city.sun['sunrise'].replace(tzinfo=None).strftime("%m/%d/%Y, %H:%M:%S"))
-    print("Sunset: " + city.sun['sunset'].replace(tzinfo=None).strftime("%m/%d/%Y, %H:%M:%S"))
+    print("Sunrise: " + s['sunrise'].replace(tzinfo=None).strftime("%m/%d/%Y, %H:%M:%S"))
+    print("Sunset: " + s['sunset'].replace(tzinfo=None).strftime("%m/%d/%Y, %H:%M:%S"))
     print("Current time: " + datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
     time.sleep(50)
