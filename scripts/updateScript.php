e80089b0f881889e0910464ca77b4f414c778c7f
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2020-08-21 18:26:16 +0200

    update updateScript.php

--- scripts/updateScript.php
+++ scripts/updateScript.php
@@ -5,16 +5,16 @@ include "connectdb.php";
 $script = explode(".", $_GET["script"]);
 
 switch($script[1]) {
-    case "py": $type = "python3";
-    case "sh": $type = "sh";
+    case "py": $type = "python3"; break;
+    case "sh": $type = "sh"; break;
     default: 
-        sendError("Not supported script type: $script.", 2); 
+        sendError("Not supported script type: " . $script[1], 2); 
         die(); 
     break;
 }
 
 if ($_GET["status"] == "true") {
-    shell_exec("nohup $type " . $_SERVER['DOCUMENT_ROOT'] . "/scripts/auto/" . $script . " >/dev/null 2>&1 &");
+    shell_exec("nohup $type " . $_SERVER['DOCUMENT_ROOT'] . "/scripts/auto/" . $script[0] . "." . $script[1] . " >/dev/null 2>&1 &");
 } else {
-    shell_exec("pkill -f " . $script);
+    shell_exec("pkill -f " . $script[0] . "." . $script[1]);
 }
