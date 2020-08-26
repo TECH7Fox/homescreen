73578254350c964a7ce8fcb9677d2985c77b1085
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2020-08-26 11:43:56 +0200

    update cams.php

--- cams.php
+++ cams.php
@@ -7,7 +7,6 @@
 
                 for ($i = 1; $i <= 4; $i++) { 
                     if ($i == 3) { echo '</div><div class="row row-50">';} ?>
-
                         <div class="col card mlr-1">
                             <h5 class="card-header"><?php echo empty($_ENV["cams"]["camera_$i"])?"None":$_ENV["cams"]["camera_$i"] ?></h5>
                             <?php
@@ -19,8 +18,8 @@
                             
                             ?>
                             <<?php echo empty($_ENV["cams"]["camera_$i"])?"img":"iframe"; ?> class="card-img-bottom camera" src="<?php echo empty($row["url"])?$kartUrl:$row["url"]; ?>"><?php echo empty($_ENV["cams"]["camera_$i"])?"":"</iframe>"; ?>
-                        </div>
-                    
+                        </div> 
+                        
                 <?php } ?>
             </div>
         </main>
